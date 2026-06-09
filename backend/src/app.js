f3915e7983a8fb9948bf0c0edc0eb0469f9d154e
kishore <kishorebalaji0307@gmail.com>
2026-06-09 12:58:26 +0530

    Allow CORS from all origins

--- backend/src/app.js
+++ backend/src/app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 // CORS
 app.use(
   cors({
-    origin: ["http://localhost:5173", "http://localhost:5174"],
+    origin: ["http://localhost:5173", "http://localhost:5174","*"],
     credentials: true,
   })
 );
